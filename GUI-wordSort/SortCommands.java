85ca6e66b931b1b2f43793d39a2a7ff3e639f812
Nicole Ellsworth <106763314+nicoleEllsworth@users.noreply.github.com>
2022-06-04 12:01:37 -0700

    Add files via upload

---
+++ GUI-wordSort/SortCommands.java
@@ -0,0 +1,297 @@
+///////////////////////////////////////////////////////////////////////////////
+// Branch:             main
+// Main Class File:    WordForm.java
+// File:               SortWords.java
+// Date:               June 2022
+//
+// Author:             Nicole  Ellsworth
+///////////////////////////////////////////////////////////////////////////////
+
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.util.*;
+
+/**
+ * The SortWords class contains all the methods for a user
+ * to create their custom sorted word list
+ *
+ * Bugs: None.
+ *
+ * @author Nicole Ellsworth.
+ */
+public class SortCommands {
+
+    // CONSTANTS
+    final static String INVALID_INPUT = "Invalid input, please try again";
+
+    // tracks current tag filter settings
+    private static ArrayList<String> currentTagList = new ArrayList<String>();
+    // tracks current character filter settings
+    private static ArrayList<Character> currentCharList = new ArrayList<Character>();
+    private static String prefix = "no prefix";
+    private static String suffix = "no suffix";
+
+    /**
+     * Calls methods from the ScrapeWords class to create
+     * a WordList object containing all the scraped words.
+     *
+     * @return void.
+     */
+    public static void setUpWords() {
+        try {
+            ReadWords.readWords();
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    // BEGIN CHARACTER LIST METHODS////////////////////////////////////////////
+
+    /**
+     * Gets current, working character list.
+     *
+     * @return ArrayList<Character> currentCharList.
+     */
+    public static ArrayList<Character> getCharList() {
+        return currentCharList;
+    }
+
+    /**
+     * Sets current, working character to new list.
+     *
+     * @return void.
+     */
+    public static void setCharList(ArrayList<Character> charlist) {
+        currentCharList = charlist;
+    }
+
+    // BEGIN TAG LIST METHODS//////////////////////////////////////////////////
+
+    /**
+     * Gets current, working taglist.
+     *
+     * @return ArrayList<String> currentTagList.
+     */
+    public static ArrayList<String> getTagList() {
+        return currentTagList;
+    }
+
+    /**
+     * Sets current, working tag list to new list.
+     *
+     * @return void.
+     */
+    public static void setTagList(ArrayList<String> tlist) {
+        currentTagList = tlist;
+    }
+
+    // BEGIN PREFIX AND SUFFX METHODS//////////////////////////////////////////
+
+    /**
+     * Gets prefix.
+     *
+     * @return prefix as a string.
+     */
+    public static String getPrefix() {
+        return prefix;
+    }
+
+    /**
+     * sets prefix to string pre.
+     *
+     * @param pre is the string to set prefix to
+     * 
+     * @return void.
+     */
+    public static void setPrefix(String pre) {
+        prefix = pre;
+    }
+
+    public static void addPrefix() {
+        if (!prefix.equals("no prefix")) {
+            FilterWords.addStartsWith(prefix);
+            FilterWords.addSoundWords();
+        }
+    }
+
+    /**
+     * Gets suffix.
+     *
+     * @return suffix as a string.
+     */
+    public static String getSuffix() {
+        return suffix;
+    }
+
+    /**
+     * sets suffix to string suff.
+     *
+     * @param suff is the string to set suffix to
+     * 
+     * @return void.
+     */
+    public static void setSuffix(String suff) {
+        suffix = suff;
+    }
+
+    public static void addSuffix() {
+        if (!suffix.equals("no suffix")) {
+            FilterWords.addEndsWith(suffix);
+            FilterWords.addSoundWords();
+        }
+    }
+
+    // BEGIN RANKING WORDS BY MATCHES//////////////////////////////////////////
+
+    /**
+     * Rates all the words in newWords by how many matches
+     * they have with current tags, characters, and prefix/suffix
+     *
+     * @return void.
+     */
+    public static void rateWords() {
+        for (Word w : FilterWords.getNewWords().getWordList()) {
+            w.setIntRate(0);
+            for (String s : currentTagList) {
+                if (w.getWordTags().contains(s)) {
+                    w.addIntRate();
+                }
+            }
+            for (char c : currentCharList) {
+                if (c == (w.getStartsWith())) {
+                    w.addIntRate();
+                }
+            }
+            if (w.getWordName().startsWith(prefix) && prefix.length() > 0) {
+                w.addIntRate();
+            }
+            String name = w.getWordName().replaceAll("tags", "").trim();
+            if (name.endsWith(suffix) && suffix.length() > 0) {
+                w.addIntRate();
+            }
+            // sets the word's Integer value of rate
+            w.setRate(w.getIntRate());
+        }
+    }
+
+    /**
+     * Sorts newWords by highest rated words to lowest
+     *
+     * @return void.
+     */
+    public static void sortWordsByMatch() {
+        rateWords();
+        Collections.sort(FilterWords.getNewWords().getWordList(),
+                new Comparator<Word>() {
+                    public int compare(Word w1, Word w2) {
+                        return w2.getRate().compareTo(w1.getRate());
+                    }
+                });
+    }
+
+    /**
+     * Sorts newWords by highest rated words only
+     * so that all words match >= m * matched tags
+     * 
+     * @param m double that determines how many
+     *          of the tags the words need to match, with
+     *          1 being the highest
+     *
+     * @return void.
+     */
+    public static void sortWordsExclusive(double m) {
+        // Total number of possible matches
+        int matchTotal = currentCharList.size() + currentTagList.size();
+        // toDelte: list of words that have less than m * matched rating
+        ArrayList<Word> toDelete = new ArrayList<Word>();
+        if (!prefix.equals("no prefix")) {
+            matchTotal += 1;
+        }
+        if (!prefix.equals("no suffix")) {
+            matchTotal += 1;
+        }
+        for (Word w : FilterWords.getNewWords().getWordList()) {
+            int r = w.getRate();
+            if (r < (matchTotal) * m) {
+                System.out.println(w.getWordName() + ", r: " + r +
+                        ", matched: " + matchTotal);
+                toDelete.add(w);
+            }
+        }
+        // loop deletes the words that don't match
+        for (Word w : toDelete) {
+            FilterWords.getNewWords().deleteWord(w);
+        }
+        // re-sort
+        SortCommands.sortWordsByMatch();
+    }
+
+    // BEGIN OTHER METHODS/////////////////////////////////////////////////////
+
+    /**
+     * Saves words to text file called "sortedWords.txt"
+     *
+     * @return void.
+     */
+    public static void saveNewWordList() {
+        // make sure list is sorted before saving
+        SortCommands.sortWordsByMatch();
+        String newLine = System.getProperty("line.separator");
+        try {
+            FileWriter myWriter = new FileWriter("sortedWords.txt");
+            myWriter.write(newLine);
+            myWriter.write("Tags: " + currentTagList);
+            myWriter.write(newLine);
+            myWriter.write("Starting letters: " + currentCharList);
+            myWriter.write(newLine);
+            myWriter.write("Prefix: " + prefix);
+            myWriter.write(newLine);
+            myWriter.write("Suffix: " + suffix);
+            myWriter.write(newLine);
+            myWriter.write(newLine);
+            myWriter.write(newLine);
+            for (Word w : FilterWords.getNewWords().getWordList()) {
+                myWriter.write(w.getWordName());
+                myWriter.write(" tags: ");
+                for (String t : w.getWordTags()) {
+                    myWriter.write(t + " ");
+                }
+                myWriter.write(newLine);
+            }
+            myWriter.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Helper method that converts ArrayList<String>
+     * to array of strings
+     *
+     * @return void.
+     */
+    public static String[] convStringArray(ArrayList<String> tags) {
+        String[] convTags = new String[tags.size()];
+        int i = 0;
+        for (String s : tags) {
+            convTags[i] = s;
+            i++;
+        }
+        return convTags;
+    }
+
+    /**
+     * Helper method that filters newWords and then
+     * sorts by ratings
+     *
+     * @return void.
+     */
+    public static void filterAndSort() {
+        FilterWords.sortByTagsAndLetters(
+                convStringArray(currentTagList), currentCharList);
+        SortCommands.sortWordsByMatch();
+    }
+
+}
---
+++ GUI-wordSort/Word.java
@@ -0,0 +1,163 @@
+///////////////////////////////////////////////////////////////////////////////
+// Branch:             main
+// Main Class File:    WordForm.java
+// File:               Word.java
+// Date:               June 2022
+//
+// Author:             Nicole  Ellsworth
+///////////////////////////////////////////////////////////////////////////////
+
+import java.util.ArrayList;
+
+/**
+ * The Word class represents some arbitrary word.
+ *
+ * Bugs: None known.
+ *
+ * @author Nicole Ellsworth.
+ */
+public class Word {
+    private String wordName;
+    private ArrayList<String> wordTags;
+    private char startsWith;
+    private Integer rate; // to rate a word's relevance to user input
+    private int rateInt;
+
+    /**
+     * no-arg constructor creates a Word instance.
+     *
+     */
+    public Word() {
+        this.wordName = "unnamed";
+        this.wordTags = new ArrayList<String>();
+        this.startsWith = '\u0000';
+        rate = 0; // tracks how many matches a word has to a users input
+        rateInt = 0;
+    }
+
+    /**
+     * Constructs a new Word instance with the specified name,
+     * tags, and first letter.
+     *
+     * @param name       The name part of this word (the word itself)
+     *                   as any string.
+     * @param wordTags   ArrayList containing attributes of the word,
+     *                   such as type and meaning.
+     * @param startsWith The first letter of the word as a char.
+     */
+    public Word(String wordName, ArrayList<String> wordTags) {
+        this.wordName = wordName;
+        this.wordTags = wordTags;
+        this.startsWith = wordName.charAt(0);
+        rate = 0;
+        rateInt = 0;
+    }
+
+    /**
+     * Gets the name of the Word instance.
+     *
+     * @return the name as a string.
+     */
+    public String getWordName() {
+        return this.wordName;
+    }
+
+    /**
+     * Gets the tags of the Word instance.
+     *
+     * @return the tags as an ArrayList.
+     */
+    public ArrayList<String> getWordTags() {
+        return wordTags;
+    }
+
+    /**
+     * adds new tags from ArrayList to word object.
+     *
+     * @return void.
+     */
+    public void addTags(ArrayList<String> tags) {
+        for (String tag : tags) {
+            this.wordTags.add(tag);
+        }
+    }
+
+    /**
+     * Gets the first letter of the Word instance.
+     *
+     * @return the first leter as a char.
+     */
+    public char getStartsWith() {
+        return startsWith;
+    }
+
+    /**
+     * Checks if word name in word list.
+     *
+     * @return boolean true if word is listed.
+     */
+    public boolean isListed(WordList words) {
+        for (Word w : words.getWordList()) {
+            if (w.getWordName().equals(this.wordName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /////////////////// STARTING RATE METHODS//////////////////////////////////
+    /**
+     * gets the Integer value of rate
+     *
+     * @return void.
+     */
+    public Integer getRate() {
+        return this.rate;
+    }
+
+    /**
+     * Helper method that converts int to Integer.
+     *
+     * @return void.
+     */
+    public static Integer valueOf​(int i) {
+        return i;
+    }
+
+    /**
+     * sets the rate to an integer value.
+     *
+     * @return void.
+     */
+    public void setRate(int num) {
+        this.rate = Integer.valueOf(num);
+    }
+
+    /**
+     * sets the mutable int value rate
+     *
+     * @return void.
+     */
+    public void setIntRate(int num) {
+        this.rateInt = 0;
+    }
+
+    /**
+     * gets the int value of rate
+     *
+     * @return void.
+     */
+    public int getIntRate() {
+        return rateInt;
+    }
+
+    /**
+     * increments the int value of rate by 1
+     *
+     * @return void.
+     */
+    public void addIntRate() {
+        this.rateInt += 1;
+    }
+
+}
